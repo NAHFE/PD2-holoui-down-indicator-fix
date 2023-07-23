0e210e20f4d536dad90636384d818a07b8075e31
NAHFE <nahfe@nahfe.xyz>
2023-07-23 19:07:30 +0200

    init

---
+++ Hooks/hudmanager.lua
@@ -0,0 +1,16 @@
+Hooks:PostHook(HUDManager, "align_teammate_panels", "NAHFEsMemesAlignPanels", function(self)
+	local function align(tm)
+		local revive_panel = tm._player_panel:child("revive_panel")
+		local name_bg = tm._panel:child("Namebg")
+		local name = tm._panel:child("name")
+		
+		revive_panel:set_x(tm._player_panel:x())
+		revive_panel:set_center_y(name_bg:y() + name_bg:h() / 2)
+		name_bg:set_left(revive_panel:w())
+		name:set_x(name_bg:x() + 4)
+	end
+	
+	for _, tm in pairs(self._teammate_panels) do
+		align(tm)
+	end
+end)
